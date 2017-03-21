364ed84280cbbe6b560ea52c64fcfca3361c68cb
tonethon <tony_park@student.wayland.k12.ma.us>
2017-03-21 13:42:28 -0400

    Created Helicopter

--- Paratroopers/src/Helicopter.java
+++ Paratroopers/src/Helicopter.java
@@ -1,19 +1,31 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
  * Created by tony_park on 3/16/17.
  */
-public class Helicopter {
-    BufferedImage pic;
-    private int picOrientation;
-    public void setPic(String heli , int orientation) {
-        try {
-            pic = ImageIO.read(new File("res/" + "Helicopter1"));
-            picOrientation = orientation;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class Helicopter extends Sprite {
+    private int dir;
+
+    public Helicopter(int x, int y, int dir) {
+        super(x, y, dir);
+        setSpeed(5);
+        setPic("Helicopter1.jpg", EAST);
     }
+
+
+
+
+//    BufferedImage pic;
+//    private int picOrientation;
+//    public void setPic(String heli , int orientation) {
+//        try {
+//            pic = ImageIO.read(new File("res/" + "Helicopter1"));
+//            picOrientation = orientation;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
