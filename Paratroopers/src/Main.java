b8236985ede6027663d373b78a3e3996eaff6767
tonethon <tony_park@student.wayland.k12.ma.us>
2017-03-21 13:45:21 -0400

    Created Helicopter

--- Paratroopers/src/Main.java
+++ Paratroopers/src/Main.java
@@ -11,28 +11,30 @@ import static javax.swing.SwingConstants.WEST;
 /**
  * Created by jackson_fletcher on 3/13/17.
  */
-public class Main extends JPanel{
-
-
-
+public class Main extends JPanel {
     public static final int FRAMEWIDTH = 800, FRAMEHEIGHT = 600;
     private Timer timer;
 
 
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Paratrooper!!!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 
-            public static void main (String[]args){
-                JFrame window = new JFrame("Paratrooper!!");
-                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
-                Main panel = new Main();
-                panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+        panel.setFocusable(true);
+        panel.grabFocus();
 
-                panel.setFocusable(true);
-                panel.grabFocus();
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
 
-                window.add(panel);
-                window.setVisible(true);
-                window.setResizable(false);
-            }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+    }
 }
